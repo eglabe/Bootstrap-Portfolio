bebdf730d7be05ec69807b1bc224e5948a9782a5
Erin <eringlabe@gmail.com>
2017-01-25 16:38:45 -0800

    updated php file

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
-<?php 
-include_once("index.html"); 
-include_once("portfolio.html"); 
-include_once("contact.html"); 
-?> 
+<?php include_once("index.html"); ?>
+
+<?php include_once("portfolio.html"); ?>
+
+<?php include_once("contact.html"); ?>
+
+<?php include_once("assests/css/style.css"); ?>
